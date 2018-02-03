40922c732436a34a132cb3aba8cdc2516e352861
Bartosz Kazuła <bartosz.kazula@stxnext.pl>
2018-02-04 00:25:40 +0100

    Better way of splitting

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,13 +1,19 @@
 package acronym
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func Abbreviate(s string) string {
 	var ac string
-	s = strings.Replace(s, "-", " ", -1)
-	words := strings.Split(s, " ")
+	words := strings.FieldsFunc(s, shouldSplit)
 	for _, word := range words {
 		ac += string(word[0])
 	}
 	return strings.ToUpper(ac)
+}
+
+func shouldSplit(r rune) bool {
+	return unicode.IsPunct(r) || unicode.IsSpace(r)
 }
